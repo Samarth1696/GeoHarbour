b4d22d58facf68b5505dd040e05b02f126f39f72
Samarth1696 <samarthrobo03@gmail.com>
2024-05-19 16:16:44 +0530

    Finally able to Deserialize WMTS

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use GeoHarbour::get_caps::{Wms, WebMappingService};
+use GeoHarbour::get_caps::{Wmts, WebMappingTileService};
 
 #[tokio::main]
 async fn main() {
     let url =
-  "https://bbmaps.itu.int/geoserver/wms?request=GetCapabilities&service=WMS&version=1.3.0".to_string();
-  let capa = WebMappingService::from_url(url.clone()).unwrap()
+  "https://bbmaps.itu.int/".to_string();
+  let capa = WebMappingTileService::from_url(url.clone()).unwrap()
         .get_capabilities().await.expect("Failure during GetCapabilities call");
+  // assert_eq!(capa.service.name, "WMS");
+  // assert_eq!(capa.service.title, "GeoServer Web Map Service");
   println!("{:?}", capa);
-  assert_eq!(capa.service.name, "WMS");
-  assert_eq!(capa.service.title, "GeoServer Web Map Service");
 }
 
